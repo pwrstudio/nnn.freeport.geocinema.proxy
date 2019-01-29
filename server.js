c19406a1bf7ff01d0915cdda00e5e7f4092a88ee
pwrstudio <cell@pwr.site>
2019-01-29 19:34:47 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const PORT = process.env.PORT || 80
+const express = require('express')
+var cors = require('cors')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  console.log('⇨ Request received')
+  console.time('run')
+
+  const remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  console.log(remoteAddress)
+
+  axios.get('http://3.208.0.37:8888/geo_fortune', {
+    headers: {
+      'x-forwarded-for': remoteAddress
+    }
+  })
+    .then(response => {
+      console.log('🛰️', ' Received data')
+      console.log(response.data)
+      res.send(response.data).end()
+      console.timeEnd('run')
+    })
+    .catch(error => console.log(error))
+
+})
+
+app.use('/', router)
+app.listen(PORT)
+console.log('NNN.FREEPORT.BRIDGE started... port:', String(PORT))
